a4b323a4e6c064a95fa0eb0b4913449808b6ff66
Oleh Lazurkevych <olehlazurkevych@gmail.com>
2017-05-13 02:20:19 +0300

    [add] Data Base

---
+++ NeoBot/NeoBot/Data_Base/GeoDataBaseUser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeoBot.Data_Base
+{
+    public class GeoDataBaseUser
+    {
+        // Properties
+        public GeoDataBase DataBase { get; set; }
+
+        private double UserLat { get; set; }
+        private double UserLon { get; set; }
+
+        // Constructors
+        public GeoDataBaseUser()
+        {
+            DataBase = new GeoDataBase();
+            DataBase.FormDataFromFile("Data.txt");
+        }
+
+        // Members
+        public void UserLocation(double latitude, double longetude)
+        {
+            UserLat = latitude;
+            UserLon = longetude;
+        }
+
+        public Path GetPoints()
+        {
+            return new Path(DataBase.FindClosest(UserLat, UserLon), UserLat, UserLon);
+        }
+    }
+
+    public class Path
+    {
+        // Properties
+        public GeoObject Place { get; set; }
+        public double StartLatitude { get; set; }
+        public double StartLongitude { get; set; }
+
+        // Constructor
+        public Path(GeoObject place, double latitude, double longitude)
+        {
+            Place = place;
+            StartLatitude = latitude;
+            StartLongitude = longitude;
+        }
+    }
+}
---
+++ NeoBot/NeoBot/Data_Base/GeoObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeoBot.Data_Base
+{
+    public class GeoObject
+    {
+        // Properties
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        // Constructors
+        public GeoObject()
+        {
+            // Empty
+        }
+
+        public GeoObject(string name, string description, double latitude, double longitude)
+        {
+            Name = name;
+            Description = description;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        // Methods
+    }
+}
